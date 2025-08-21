ad74fbad3e3e4421bbc7572bb06bce5963e94fef
praneethramk <120415198+praneethramk@users.noreply.github.com>
2025-08-21 18:08:37 +0530

    Check Router Configuration and Start Dev Server

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { Toaster } from "@/components/ui/sonner";
-import { WalletProvider } from "@/contexts/WalletProvider";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WalletProvider>
-      <BrowserRouter>
-        <App />
-        <Toaster />
-      </BrowserRouter>
-    </WalletProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
